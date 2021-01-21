f214006097ffda689188ecbbaa5cd4567313955b
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-01-21 12:25:00 -0500

    Scrolling and wrapping fixees on editor

--- main.js
+++ main.js
@@ -1,86 +1,3 @@
-var placeholderText = "bobby-booy\rbilly booooooy";
-var currentGutter;
-var currentLine; //int (representing the index # of the line in the lines and gutter arrays)
-
-function setGutterArray() {
-    //There will always be the number 1 in the gutter.
-    //Remove all elements in the gutter container in HTML file
-    //Using the count of lines (gotten from text area scrollHeight / gutter-number height), recursicvely add numbers to to the gutter container in the html, adding the proper class
-    let workingGutter = [];
-    currentGutter = [];
-    let lineHeight = $(".gutter-number").height();
-    let textHeight = $('textarea:first').prop('scrollHeight');
-    let numNumbers = Math.floor(textHeight/lineHeight);
-    for(let i = 1; i <= numNumbers; i++) {
-        workingGutter.push(i);
-    }
-    currentGutter = workingGutter;
-}
-
-function displayGutter() {
-    //Remove all child elements of #gutter excet "g1"
-    $("#gutter").empty();
-    //Set new child elements
-    if(currentGutter.length > 1) {
-        let p1 = '<div id="';
-        let idTag;
-        let p2 = '" class="gutter-number">';
-        let content;
-        let p3 = "</div>"
-        for(let i = 0; i < currentGutter.length; i++) {
-            content = i+1;
-            idTag = "g" + content;
-            let appendString = p1 + idTag + p2 + content + p3;
-            $("#gutter").append(appendString);
-        }
-    }
-}
-
-/*function setCurrentLine(l) {
-    currentLine = l;
-    highlightLine(l);
-}
-
-function highlightLine(l) {
-    //don't forget to unhighlight every other line
-    //l is the passed in idex of the proper line/gutter array element
-    //RESEARCH FOR THIS :(
-}*/
-
-/*function sizeStuff() {
-    let lineHeight = $(".gutter-number").height();
-    let leftOffset = $("").offset();
-    $("#highlighter").css("height", lineHeight);
-}*/
-
-function editorHandling() {
-    autoGrowTextArea();
-    setGutterArray();
-    displayGutter();
-    //setCurrentLine(l);
-}
-
-function autoGrowTextArea() {
-    $("#editor-textarea").height("5px");
-    let needed = $('textarea:first').prop('scrollHeight');
-    $("#editor-textarea").css("height", needed);
-}
-
-function preparePage() {
-    //Getting Footer Menu Ready
-    $("#settings-box").hide();
-    //Set placeholder text
-    $("#editor-textarea").val(placeholderText);
-    editorHandling();
-}
-
-
-
-
-
-
-
-
 $(document).ready(function() {
     preparePage();
     $('#settings-button').click(function() {
@@ -94,5 +11,99 @@ $(document).ready(function() {
         $('#settings-button').css("box-shadow", "none");
     });
     $("#editor-textarea").on('input', (event) => editorHandling());
-
 });
+
+
+
+
+var placeholderText = "bobby-booy\rbilly booooooy";
+var currentGutter;
+var currentLine; //int (representing the index # of the line in the lines and gutter arrays)
+var lastLine = 0;
+
+
+function preparePage() {
+    //Getting Footer Menu Ready
+    $("#settings-box").hide();
+    //Set placeholder text
+    $("#editor-textarea").val(placeholderText);
+    editorHandling();
+}
+
+function editorHandling() {
+    autoGrowTextArea();
+    setGutterArray();
+    displayGutter();
+    //highlighterHandling();
+}
+
+/*function highlighterHandling() {
+    fetchHighlightHeight();
+    moveHighlighter();
+}*/
+
+function autoGrowTextArea() {
+    $("#editor-textarea").height("5px");
+    let needed = $('textarea:first').prop('scrollHeight');
+    $("#editor-textarea").css("height", needed);
+}
+
+function setGutterArray() {
+    //There will always be the number 1 in the gutter.
+    //Remove all elements in the gutter container in HTML file
+    //Using the count of lines (gotten from text area scrollHeight / gutter-number height), recursicvely add numbers to to the gutter container in the html, adding the proper class
+    currentGutter = [];
+    let lineHeight = $(".gutter-number").height();
+    let textHeight = $('textarea:first').prop('scrollHeight');
+    let numNumbers = Math.floor(textHeight/lineHeight);
+    for(let i = 1; i <= numNumbers; i++) {
+        currentGutter.push(i);
+    }
+}
+
+function displayGutter() {
+    //Remove all child elements of #gutter excet "g1"
+    $("#gutter").empty();
+    //Set new child elements
+    let p1 = '<div id="';
+    let idTag;
+    let p2 = '" class="gutter-number">';
+    let content;
+    let p3 = "</div>";
+    if(currentGutter.length > 1) {
+        for(let i = 0; i < currentGutter.length; i++) {
+            content = i+1;
+            idTag = "g" + content;
+            let appendString = p1 + idTag + p2 + content + p3;
+            $("#gutter").append(appendString);
+        }
+    } else {
+        content = "1";
+        idTag = "g1";
+        let appendString = p1 + idTag + p2 + content + p3;
+        $("#gutter").append(appendString);
+    }
+}
+
+/*function fetchHighlightHeight() {
+    var linesData = $("#editor-textarea").val();
+    let lines = linesData.split(/\r?\n/);
+    let cursorIndex = Math.floor($("#editor-textarea")[0].selectionStart);
+    var lineI = 0; //Represents which line is to be highlighted
+    let lineFound = false;
+    while(cursorIndex > 0 && !lineFound) {
+        if(cursorIndex > lines[lineI].length) {
+            cursorIndex -= lines[lineI].length;
+            lineI += 1;
+        } else {
+            lineFound = true;
+        }
+    }
+    lastLine = currentLine;
+    currentLine = lineI + 1;
+    alert(currentLine);
+}*/
+
+/*function moveHighlighter() {
+
+}*/
